f2d5bbf6bda7bbd1b9c95a73d0c933424aa16006
Elliot Burn <elliotburn@ymail.com>
2019-04-28 14:36:41 +0000

    its putting in scans and prints adding menu

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 int main()
 {
-   printf("Enter the number of task to perform:\n1:Encrypt a message with rotational cypher with key.\n2:Decrypt rotational cypher with key.\n3:Encrypt a message with substitution cypher with key.\n4:Decrypt substitution cypher with key.\n5:Decrypt rotation cypher without key.\n");   
-    int option = 5;
+    int option;
+    int didItWork;
+    char aCommonLetter[] = "ETAOINSRHLDCUMFPGWYBVKXJQ";
+    char letter = 0;
+    do 
+    {
+    printf("Enter the number of task to perform:\n1:Encrypt a message with rotational cypher with key.\n2:Decrypt rotational cypher with key.\n3:Encrypt a message with substitution cypher with key.\n4:Decrypt substitution cypher with key.\n5:Decrypt rotation cypher without key.\n");   
+    scanf("%d", &option);
+    //option = 5;
+    }
+    while(option > 5 || option < 1);
+
     switch (option)
     {
         case 1:
@@ -100,43 +110,56 @@ int main()
         }
         case 5:
         {
-            char encryption[] = "GEBGOC";            
-            int length = 1;
-            while (encryption[length])
-                length++;
-            char message[length];
-            int currentCount = 0;
-            int highestCount = 0;
-            int letterCounted = 0;
-            for (int index = 0; index < length; index++)
-            {
-                if (currentCount > highestCount)
-                {    
-                    letterCounted = encryption[index - 1];
-                    highestCount = currentCount;
-                }
-                currentCount = 0;
-                for (int count = 0; count < length; count++)
+           do
+           {
+                char encryption[] = "GEBGOC";            
+                int length = 1;
+                while (encryption[length])
+                    length++;
+                char message[length];
+                int currentCount = 0;
+                int highestCount = 0;
+                int letterCounted = 0;
+                for (int index = 0; index < length; index++)
                 {
-                    if (encryption[count] == encryption[index])
-                        currentCount++;
+                    if (encryption[index] > 96 && encryption[index] < 123)
+                        encryption[index] = encryption[index] - 32;
+                    if (encryption[index] > 64 && encryption[index] < 91)
+                    {        
+                        if (currentCount > highestCount)
+                        {    
+                            letterCounted = encryption[index - 1];
+                            highestCount = currentCount;
+                        }
+                        currentCount = 0;
+                        for (int count = 0; count < length; count++)
+                        {
+                            if (encryption[count] == encryption[index])
+                            currentCount++;
+                        }
+                    }
+
                 }
-            }
-            int key = letterCounted - 69;
-            if (key > 0)
-                key = key - 26;
-            for (int index = 0; index < length; index++)
-            {
-                message[index] = encryption[index];
-                if (message[index] > 96 && message[index] < 123)
-                    message[index] = message[index] - 32;
-                if (message[index] > 64 && message[index] < 91)
+                int key = letterCounted - aCommonLetter[letter];
+                if (key > 0)
+                    key = key - 26;
+                for (int index = 0; index < length; index++)
                 {
-                   message[index] = message[index] - 65;
-                   message[index] = (message[index] - key) % 26 + 65;
+                    message[index] = encryption[index];
+                    if (message[index] > 96 && message[index] < 123)
+                        message[index] = message[index] - 32;
+                    if (message[index] > 64 && message[index] < 91)
+                    {
+                        message[index] = message[index] - 65;
+                        message[index] = (message[index] - key) % 26 + 65;
+                    }
                 }
+                printf("Is this the decrypted message?: %s\na) Yes\nb) No" , message);
+                scanf("%c", &didItWork - 97);
+                if (didItWork)
+                    ++;
             }
-            printf("This is possibly the decrypted message, if the most common letter is E: %s\n", message);
+            while(didItWork);
             break;
         }
         default: printf("Command not recognised.");
